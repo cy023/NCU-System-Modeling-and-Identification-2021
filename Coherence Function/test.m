fe8dd43217e9901c30f1f40c23b9b43b060886ab
cyyang <cyyang@g.ncu.edu.tw>
2021-05-26 13:02:44 +0800

    Feat new ploting format

--- Coherence Function/test.m
+++ Coherence Function/test.m
@@ -30,16 +30,16 @@ L = size(u_rand,2);  % number of sampLe
 [Y, y_rand] = responseWithNoise(Ad, Bd, Cd, Dd, u_rand);
 
 %%
-figure(1)
-subplot(2,1,1)
+figure();
+subplot(3,1,1);
 plot(u_rand(1,:)); hold on
 plot(u_rand(2,:)); hold off
-title('u\_rand')
+title("u\_rand");
 
-subplot(2,1,2)
+subplot(3,1,2);
 plot(y_rand(1,:)); hold on
 plot(y_rand(2,:)); hold off
-title('y\_rand')
+title("y\_rand");
 
 %%
 freq = linspace(0, fs/2, L/2);
@@ -48,27 +48,20 @@ y2_fft = abs(fft(y_rand(2, :)));
 y1_fft = y1_fft(1:L/2);
 y2_fft = y2_fft(1:L/2);
 
-figure(2);
-subplot(2,1,1);
-plot(freq, y1_fft);
+subplot(3,1,3);
+plot(freq, y1_fft); hold on
+plot(freq, y2_fft); hold off
 grid on;
-title("output1 random exitation");
+legend("output 1", "output 2");
 xlabel("frequency (Hz)");
 ylabel("Amplitude");
+title("output random exitation");
 
-subplot(2,1,2);
-plot(freq, y2_fft);
-grid on;
-title("output2 random exitation");
-xlabel("frequency (Hz)");
-ylabel("Amplitude");
-
-%%
+%% Do circular correlation algorithm, R_yu and R_uu in each section
 chop_Y = reshape(     Y, [q, 2*L/chops, chops]);
 chop_U = reshape(u_rand, [m,   L/chops, chops]);
 chop_y = reshape(y_rand, [q,   L/chops, chops]);
 
-% Do circular correlation algorithm, R_yu and R_uu in each section
 N = 1 + (L/chops-1)*2;
 chop_Ryu = zeros([m, N, chops]);
 chop_Ruu = zeros([q, N, chops]);
@@ -91,125 +84,106 @@ flat_Su  = reshape(chop_Su,  [q, L]);
 
 time = linspace(0, N*chops*Ts, N*chops);
 
-figure(3);
+%
+figure();
 subplot(2,1,1);
-plot(time, abs(flat_Ryu(1, :))); grid on;
-title('flat R_{yu}1');
+plot(time, abs(flat_Ryu(1, :))); hold on
+plot(time, abs(flat_Ryu(2, :))); hold off
+grid on;
+legend("flat R_{yu}(1)", "flat R_{yu}(2)");
+title("flat R_{yu}");
 
 subplot(2,1,2);
-plot(time, abs(flat_Ryu(2, :))); grid on;
-title('flat R_{yu}2');
+plot(time, abs(flat_Ruu(1, :))); hold on
+plot(time, abs(flat_Ruu(2, :))); hold off
+grid on;
+legend("flat R_{uu}(1)", "flat R_{uu}(2)");
+title("flat R_{uu}");
 
-figure(4);
+%
+figure();
 subplot(2,1,1);
-plot(time, abs(flat_Ruu(1, :))); grid on;
-title('flat R_{uu}1');
+plot(abs(flat_Sy(1, :))); hold on
+plot(abs(flat_Sy(2, :))); hold off
+grid on;
+legend("flat S_{y}(1)", "flat S_{y}(2)");
+title("flat S_{y}");
 
 subplot(2,1,2);
-plot(time, abs(flat_Ruu(2, :))); grid on;
-title('flat R_{uu}2');
-
-figure(5);
-subplot(2,1,1);
-plot(abs(flat_Sy(1, :))); grid on;
-title('flat S_{y}1');
-
-subplot(2,1,2);
-plot(abs(flat_Sy(2, :))); grid on;
-title('flat S_{y}2');
-
-figure(6);
-subplot(2,1,1);
-plot(abs(flat_Su(1, :))); grid on;
-title('flat S_{u}1');
-
-subplot(2,1,2);
-plot(abs(flat_Su(2, :))); grid on;
-title('flat S_{u}2');
+plot(abs(flat_Su(1, :))); hold on
+plot(abs(flat_Su(2, :))); hold off
+grid on;
+legend("flat S_{u}(1)", "flat S_{u}(2)");
+title("flat S_{u}");
 
 %%
 mean_Ryu = mean(chop_Ryu(:, 1:N, :), 3);
 mean_Ruu = mean(chop_Ruu(:, 1:N, :), 3);
 time = linspace(0, N*Ts, N);
 
-figure(7);
+%
+figure();
 subplot(2,1,1);
-plot(time, mean_Ruu(1, :)); grid on;
-title('Mean of R_{uu}1');
+plot(time, mean_Ruu(1, :)); hold on
+plot(time, mean_Ruu(2, :)); hold off
+grid on;
+legend("R_{uu}(1)", "R_{uu}(2)");
 xlabel("time (s)");
 ylabel("Value");
+title("Mean of R_{uu}");
 
 subplot(2,1,2);
-plot(time, mean_Ruu(2, :)); grid on;
-title('Mean of R_{uu}2');
+plot(time, mean_Ryu(1, :)); hold on
+plot(time, mean_Ryu(2, :)); hold off
+grid on;
+legend("R_{yu}(1)", "R_{yu}(2)");
 xlabel("time (s)");
 ylabel("Value");
+title("Mean of R_{yu}");
 
-figure(8);
-subplot(2,1,1);
-plot(time, mean_Ryu(1, :)); grid on;
-title('Mean of R_{yu}1');
-xlabel("time (s)");
-ylabel("Value");
-
-subplot(2,1,2);
-plot(time, mean_Ryu(2, :)); grid on;
-title('Mean of R_{yu}2');
-xlabel("time (s)");
-ylabel("Value");
 
 %%
 mean_Sy = mean(chop_Sy(:, :, :), 3);
 mean_Su = mean(chop_Su(:, :, :), 3);
 freq = linspace(0, fs/2, L/chops);
 
-figure(9);
+%
+figure();
 subplot(2,1,1);
-plot(freq, abs(mean_Su(1, :))); grid on;
-title('Mean of density spectrum S_{uu}1');
+plot(freq, abs(mean_Su(1, :))); hold on
+plot(freq, abs(mean_Su(2, :))); hold off
+grid on;
+legend("S_{uu}(1)", "S_{uu}(2)");
 xlabel("frequency (Hz)");
 ylabel("Amplitude");
+title("Mean of density spectrum");
 
 subplot(2,1,2);
-plot(freq, abs(mean_Su(2, :))); grid on;
-title('Mean of density spectrum S_{uu}2');
+plot(freq, abs(mean_Sy(1, :))); hold on
+plot(freq, abs(mean_Sy(2, :))); hold off
+grid on;
+legend("S_{yu}(1)", "S_{yu}(2)");
 xlabel("frequency (Hz)");
 ylabel("Amplitude");
-
-figure(10);
-subplot(2,1,1);
-plot(freq, abs(mean_Sy(1, :))); grid on;
-title('Mean of density spectrum S_{yu}1');
-xlabel("frequency (Hz)");
-ylabel("Amplitude");
-
-subplot(2,1,2);
-plot(freq, abs(mean_Sy(2, :))); grid on;
-title('Mean of density spectrum S_{yu}2');
-xlabel("frequency (Hz)");
-ylabel("Amplitude");
+title("Mean of density spectrum");
 
 %%
 Gz = mean_Sy .* (mean_Su.^-1);
 
-figure(11);
-subplot(2,1,1);
-plot(freq, abs(Gz(1, :))); grid on;
-title('Mean of density spectrum G_z1');
-
-subplot(2,1,2);
-plot(freq, abs(Gz(2, :))); grid on;
-title('Mean of density spectrum G_z2');
+figure();
+plot(freq, abs(Gz(1, :))); hold on
+plot(freq, abs(Gz(2, :))); hold off
+grid on;
+legend("G_z1", "G_z2");
+title("Mean of density spectrum");
 
 %%
 y_ifft = [ifft(Gz(1, :)); ifft(Gz(2, :))];
 t      = linspace(0, Ts*L/chops, L/chops);
 
 figure();
-subplot(2,1,1);
-plot(t, abs(y_ifft(1, :))); grid on;
-title('New Markov parameters Y^*_1');
-
-subplot(2,1,2);
-plot(t, abs(y_ifft(2, :))); grid on;
-title('New Markov parameters Y^*_2');
+plot(t, abs(y_ifft(1, :))); hold on
+plot(t, abs(y_ifft(2, :))); hold off
+grid on;
+legend("Y^*_1", "Y^*_2");
+title("New Markov parameters");
